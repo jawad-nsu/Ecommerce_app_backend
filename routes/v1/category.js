7ae61a0dab256097e09017124bd134b28d512fa6
jawad-nsu <jawadurrahim1997@gmail.com>
2022-10-14 23:52:15 +0600

    protect category route

--- routes/v1/category.js
+++ routes/v1/category.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { requireSignin, isUser, isAdmin } = require('../../controllers/auth');
 const { create } = require('../../controllers/category');
+const { userById } = require('../../controllers/user');
 const router = express.Router();
 
-router.post('/category/create', create);
+router.post('/category/create/:userId', requireSignin, isUser, isAdmin, create);
+
+router.param('userId', userById);
 
 module.exports = router;
