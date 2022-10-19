a737795ac9e0eb78926dea107010ef3a0a7e1041
jawad-nsu <jawadurrahim1997@gmail.com>
2022-10-19 09:48:18 +0600

    product model added

---
+++ models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
+    description: {
+      type: String,
+      maxLength: 2000,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      maxLength: 32,
+    },
+    quantity: {
+      type: Number,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Product', productSchema);
