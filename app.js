d6b91f67ae763e4187cf545bdc45f8f30abbdbfe
jawad-nsu <jawadurrahim1997@gmail.com>
2022-10-30 13:25:49 +0600

    added product route to app

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const expressValidator = require('express-validator');
 const auth = require('./routes/v1/auth');
 const user = require('./routes/v1/user');
 const category = require('./routes/v1/category');
+const product = require('./routes/v1/product');
 
 const PORT = process.env.PORT || 8000;
 
@@ -32,6 +33,7 @@ app.use(expressValidator());
 app.use('/api/v1', auth);
 app.use('/api/v1', user);
 app.use('/api/v1', category);
+app.use('/api/v1', product);
 
 app.listen(PORT, () => {
   consola.success('Listening on port 8000 updated');
