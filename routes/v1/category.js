af25e56e75eab436b0da262ff9b8e5d7c3460b72
jawad-nsu <jawadurrahim1997@gmail.com>
2023-01-02 19:16:20 -0500

    category update, delete, all categories controller

--- routes/v1/category.js
+++ routes/v1/category.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignin, isUser, isAdmin } = require('../../controllers/auth');
-const { create, categoryById, read } = require('../../controllers/category');
+const {
+  create,
+  categoryById,
+  read,
+  lists,
+  update,
+  remove,
+} = require('../../controllers/category');
 const { userById } = require('../../controllers/user');
 
 router.post('/category/create/:userId', requireSignin, isUser, isAdmin, create);
 router.get('/category/:categoryId', read);
+router.get('/categories', lists);
+router.put('/category/:categoryId', requireSignin, isUser, isAdmin, update);
+router.delete('/category/:categoryId', requireSignin, isUser, isAdmin, remove);
 
 router.param('userId', userById);
 router.param('categoryId', categoryById);
