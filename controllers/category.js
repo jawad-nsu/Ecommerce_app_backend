7492e9e0c8b1c26909448cc50514fcb4cce8a051
jawad-nsu <jawadurrahim1997@gmail.com>
2023-01-02 19:15:33 -0500

    added category update, delete and all get categories routes

--- controllers/category.js
+++ controllers/category.js
@@ -15,6 +15,42 @@ exports.read = (req, res) => {
   res.status(200).json(req.category);
 };
 
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+exports.lists = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    return res.json(data);
+  });
+};
+
+exports.remove = (req, res) => {
+  const category = req.category;
+  category.delete((err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: errorHandler(err),
+      });
+      res.json('Category deleted');
+    }
+  });
+};
+
 exports.categoryById = (req, res, next, id) => {
   Category.findById(id).exec((err, category) => {
     if (err || !category) {
